786b12a9dfbc14a1de97caa6d528876529c2ed9d
Sakonwatzzz <650112418026@bru.ac.th>
2024-01-21 02:15:08 +0700

    Project-ftp

--- website-php/index.php
+++ website-php/index.php
@@ -4,39 +4,50 @@ session_start();
 
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="style.css">
     <title>FTP File Upload</title>
 </head>
-<body>
-    <h2>Uploaded Files</h2>
 
-    <?php
-    if (isset($_SESSION['uploadedFiles']) && !empty($_SESSION['uploadedFiles'])) {
-        echo "<p>Uploaded files:</p>";
-        echo "<ul>";
-        foreach ($_SESSION['uploadedFiles'] as $filename) {
-            echo "<li id='file_$filename'>
+<body>
+    <header>
+        <h2 class="h-ftp">FTP Files</h2>
+
+    </header>
+
+    <nav>
+        <?php
+        if (isset($_SESSION['uploadedFiles']) && !empty($_SESSION['uploadedFiles'])) {
+            echo "<p>Uploaded files:</p>";
+            echo "<ul>";
+            foreach ($_SESSION['uploadedFiles'] as $filename) {
+                echo "<li id='file_$filename'>
                     <span id='display_$filename'>$filename</span> <tr>
-                    <a href='download.php?file=" . urlencode($filename) . "'>Download File</a> <tr>
+                    <a href='download.php?file="  . urlencode($filename) . "'>Download File</a> <tr>
                     <a href='delete.php?file=" . urlencode($filename) . "'>Delete File</a> <tr>
                     <a href='#' onclick='editFileName(\"$filename\")'>Edit Name</a> <tr>
                   </li>";
+            }
+            echo "</ul>";
+        } else {
+            echo "<p>No files uploaded yet.</p>";
         }
-        echo "</ul>";
-    } else {
-        echo "<p>No files uploaded yet.</p>";
-    }
-    ?>
-
-    <h2>Upload File to FTP</h2>
+        ?>
+    </nav>  
+<article>
+    <h2 class="h-uftof">Upload File to FTP</h2>
     <form action="upload.php" method="post" enctype="multipart/form-data">
-        <label for="file">Select a file:</label>
+        <label class="la-select" for="file">Select a file:</label>
         <input type="file" name="file" id="file">
         <br>
-        <input type="submit" value="Upload">
+        <input class="int-up" type="image" src="https://cdn-icons-png.flaticon.com/512/3616/3616929.png" width=50px height 50px="submit" value="Upload">
     </form>
+</article>
+
 
     <script>
         function editFileName(filename) {
@@ -48,4 +59,5 @@ session_start();
         }
     </script>
 </body>
+
 </html>
